af4d7f6918a627d29b977c254a430a1a3c289572
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-04-26 03:24:19 +0100

    Correction

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,33 +1,33 @@
 -- Creates a stored procedure that computes average weighted score for a student
-DELIMITER / /
-
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser
-(IN user_id INT)
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-DECLARE
-	total_score FLOAT DEFAULT 0;
-DECLARE
-	total_weight INT DEFAULT 0;
-DECLARE
-	weighted_score FLOAT;
-	-- Calculate total weighted score
-	SELECT SUM(score * weight) INTO total_score
-	FROM corrections
-	    JOIN projects ON corrections.project_id = projects.id
-	WHERE
-	    corrections.user_id = user_id;
-	-- Calculate total weight
-	SELECT SUM(weight) INTO total_weight FROM projects;
-	-- Calculate average weighted score
-	IF total_weight > 0 THEN
-	SET
-	    weighted_score = total_score / total_weight;
-	ELSE SET weighted_score = 0;
-END
-	IF;
-	-- Update average_score for the user
-	UPDATE users SET average_score = weighted_score WHERE id = user_id;
-END
-//
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
 
-DELIMITER;
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
+    END IF;
+END $$
+DELIMITER ;

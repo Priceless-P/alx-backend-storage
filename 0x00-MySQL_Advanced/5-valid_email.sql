14ee589ca9604888f49260509b51d33407136489
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-04-26 09:36:22 +0100

    correction

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -8,9 +8,9 @@ CREATE TRIGGER email_verify
 BEGIN
     IF OLD.email != NEW.email
     THEN
-        SET NEW.valid_email = 0
+        SET NEW.valid_email = 0;
     ELSE
-        SET NEW.valid_email = NEW.valid_email
+        SET NEW.valid_email = NEW.valid_email;
     END IF;
 END $$
 DELIMITER ;

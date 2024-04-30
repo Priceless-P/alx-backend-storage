a1e562b0bafa46c52e09df42f59efb62712e2a1c
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-04-30 21:03:31 +0100

    correction

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -4,28 +4,35 @@ from pymongo import MongoClient
 from collections import Counter
 
 
-def get_log_stats(nginx_collection):
-    """Provides some stats about Nginx
-    logs stored in MongoDB"""
-    logs = nginx_collection.count_documents({})
-    print(f"{logs} logs")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+def parse_log():
+    """
+    Provides some stats about
+    Nginx logs stored in MongoDB
+    """
+    client = MongoClient('localhost', 27017)
+    db = client.logs
+    nginx_collection = db.nginx
+
+    total_logs = nginx_collection.count_documents({})
+
+    print(f"{total_logs} logs")
+
     print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    for method_ in methods:
-        count = nginx_collection.count_documents({"method": method_})
-        print(f"\tmethod {method_}: {count}")
+    count_status = nginx_collection.count_documents({"method": "GET",
+                                                    "path": "/status"})
+    print(f"{count_status} status check")
 
-    status_check = nginx_collection.count_documents(
-                                    {"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
-
-    count_ips = Counter(doc['ip'] for doc in nginx_collection.find({}, {"ip":1}))
+    count_ips = Counter(doc['ip'] for doc in nginx_collection.
+                        find({}, {"ip": 1}))
     top_ten = count_ips.most_common(10)
     for ip, count in top_ten:
         print("      {}: {}".format(ip, count))
 
+
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-    get_log_stats(nginx_collection)
+    parse_log()
